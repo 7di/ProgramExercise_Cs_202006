b76868c8c6b5c10a2201844a2a2478a40e9bea0c
Cs <1453749520@qq.com>
2020-06-21 17:02:08 +0800

    finish GuessNumber  input diffierent numbers

--- src/main/java/com/twschool/practice/GuessNumber.java
+++ src/main/java/com/twschool/practice/GuessNumber.java
@@ -22,34 +22,38 @@ public class GuessNumber {
         int gameTimes = 0;//单局游戏次数
         String userAnswer1 = null;
         GuessNumber answer = new GuessNumber();
-        answer.answerNumbers = Arrays.asList("1", "2", "3", "4");
+        GuessNumber.answerNumbers = Arrays.asList("1", "2", "3", "4");
         ArrayList<String> userAnswer = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
-        System.out.println("请输入4个0-9整数，互相之间不能重复：");
-        while (!answer.answerNumbers.toString().equals(userAnswer.toString())) {
 
+        while (!GuessNumber.answerNumbers.toString().equals(userAnswer.toString())) {
             userAnswer.clear();
             valueAndPositionCorrectCount = 0;
             positionCorrectCount = 0;
-            // System.out.println("useranswer is" + userAnswer.toString());
+            System.out.println("请输入4个0-9整数，互相之间不能重复：");
             for (int i = 0; i < 4; i++) {
                 userAnswer.add(sc.next());
             }
+            if(answer.isRepeat(userAnswer)){
+                userAnswer.clear();
+            }
+            else{
             //System.out.println("用户初始输入"+ userAnswer.toString());
-            userAnswer1 = userAnswer.toString();//需要去掉字符串中的括号及逗号
-            userAnswer1 = userAnswer1.replace("[", "").replace("]", "").replace(",", "");
+            //需要去掉字符串中的括号及逗号
+            userAnswer1 = userAnswer.toString().replace("[", "").replace("]", "").replace(",", "");
             //System.out.println("替换后"+ userAnswer1);
             String result = answer.check(userAnswer1);
             if (result.equals("4A0B")) {
                 System.out.println(result + "        Congratulations，you win！");
                 System.exit(0);
             } else if (gameTimes < 5) {
-                System.out.println(result + "        Sorry,please try again!");
+                System.out.println(result + "        Sorry,please try again! The left game times：" + (5-gameTimes));
             } else {
-                System.out.println("Sorry，you have failed！");
+                System.out.println("Sorry，you have failed！The right answer is “1 2 3 4”");
                 System.exit(0);
             }
             gameTimes++;
+        }
         }
     }
 
@@ -65,5 +69,13 @@ public class GuessNumber {
             }
         }
         return valueAndPositionCorrectCount + "A" + positionCorrectCount + "B";
+    }
+    public boolean isRepeat( List<String> listname){
+        long count = listname.stream().distinct().count();
+        if(count < listname.size()){
+            System.out.println("Warning! There exsit some repeat number,please input diffierent number:");
+            return true;
+        }
+        return false;
     }
 }
